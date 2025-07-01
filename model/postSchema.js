2534d4fe20c66d540be70e825461edddd71e654d
Aman Reja <amnmolla742@gmail.com>
2025-07-01 08:53:25 +0530

    fix

--- model/postSchema.js
+++ model/postSchema.js
@@ -3,6 +3,8 @@ const dataSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
     content: { type: String, required: true },
+    postimage: { type: String },
+    postimageurl: { type: String },
   },
   { timestamps: true }
 );
